45aae7eca848e56d031b34fd3a052a56dfe1bcbd
jayshree-dolase <jayshreedolase3@gmail.com>
2024-06-03 22:10:06 +0530

    3rd Commit

--- src/User/Main.java
+++ src/User/Main.java
@@ -1,8 +1,8 @@
-package User;
+ package User;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws java.sql.SQLException {
 		
 		Show_Option show = new Show_Option();
 		show.optionDetails();
